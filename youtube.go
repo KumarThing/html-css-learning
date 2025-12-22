506412cab4f5f87c156920863ed5329662729dc2
KumarThing <102548843+KumarThing@users.noreply.github.com>
2025-12-22 15:19:12 +0200

    go gile for youtube

---
+++ youtube.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("youtube_clone/index.html"))
+
+func main() {
+	http.Handle("/youtube_clone/", http.StripPrefix("/youtube_clone/", http.FileServer(http.Dir("style.css"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		tmpl.Execute(w, nil)
+	})
+
+	fmt.Println("your server is running in http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+
+}
