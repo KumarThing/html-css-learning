dc1db32dfabf76ce15c9d1600482984f0d55d650
KumarThing <102548843+KumarThing@users.noreply.github.com>
2025-12-23 16:05:26 +0200

    main.go for youtube

--- youtube.go
+++ youtube.go
@@ -9,7 +9,7 @@ import (
 var tmpl = template.Must(template.ParseFiles("youtube_clone/index.html"))
 
 func main() {
-	http.Handle("/youtube_clone/", http.StripPrefix("/youtube_clone/", http.FileServer(http.Dir("style.css"))))
+	http.Handle("/youtube_clone/static/", http.StripPrefix("/youtube_clone/static/", http.FileServer(http.Dir("youtube_clone/static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		tmpl.Execute(w, nil)
